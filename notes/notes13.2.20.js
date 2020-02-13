6fe9f842c00b449c4dd91ea5c73a28080c4ced37
jdbogle1 <jam.d.bogle@gmail.com>
2020-02-13 10:41:04 +0000

    added notes

---
+++ notes/notes13.2.20.js
@@ -0,0 +1,24 @@
+/*
+just because it looks like a file path doesn't mean it physically is
+
+URL Routing = using code to navigate the url accordingly and makes sure you end up with error 404 if the inputed url doesn't exist
+
+tld = top level domain e.g .com .co.uk .org
+main domain, what's left of the tld
+sub domain, to the left of the main domain
+domains seperated by '.'
+
+in a website '?' begins a query
+'&' connects together multiple queries
+deep links allow you to get the same page every time
+
+
+
+
+
+
+
+
+
+
+*/
