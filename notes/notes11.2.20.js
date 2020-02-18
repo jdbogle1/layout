19126faeef5b4470ab82dc3006c889d30de8eb15
jdbogle1 <jam.d.bogle@gmail.com>
2020-02-18 12:00:30 +0000

    everything

--- notes/notes11.2.20.js
+++ notes/notes11.2.20.js
@@ -1,7 +1,26 @@
 //Notes
 
 /*
+Knowledge module: 2 exams (programming + behaviours and practices)
+Open book, no help
+
+Synoptic project
+Get observed for 3-5 days
+
+Need workplace evidence
+British Computing Society defines the standards of the apprenticeship
 
 
+Your daily stamina as a developer is something you want to improve.
+How much stamina do I have?
 
+
+Learn using PBL (Problem Based Learning): Learn by solving problems rather than teaching
+
+What is the flow?
+Any new system you use, you have to know the flow
+
+HTML (Main file =index.html)
+Comments in javascript = //
+		HTML= #
 */
---
+++ notes/notes18.2.20.js
@@ -0,0 +1,42 @@
+/*
+UI and UX is about what the user wants and needs
+
+Questions to ask:
+How can we reduce the amount of work needed by the user?
+How can we reduce screen clutter?
+How can we increase the speed of the app?
+
+Possible features:
+Access shortcuts
+Autosave
+Dark Mode
+Adjustable Font
+
+
+TERMINAL
+cd = Change Directory
+dir = directory
+cd .. = go back out 1 level
+up key = goes through command history
+
+
+Navigating directory
+..\[preferred directory]
+
+HT access - allows you to control whatever the browser reads, invisible file in the server
+
+ctrl + / = comments what is highlited
+
+selector = name of the class/id
+
+css is nortmally applied in order of being loaded but there are certain priorities (id>class)
+
+!important increases priority
+
+1. Has it been linked correctly? - is it available and known
+2. Has it been targeted correctly - correct selector e.g. .class and #id
+3. Has it been overridden e.g. !important
+
+
+
+*/
